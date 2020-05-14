96ac07d48981d56fcbd0cfe11f9fb2d1e0ce6918
Maks <groover226@yandex.ru>
2020-05-14 10:23:46 +0300

    React

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Robot 1',
+        username: 'Robobo1',
+        email: 'Robot1@robot.com'
+    },
+    {
+        id: 2,
+        name: 'Robot 2',
+        username: 'Robobo2',
+        email: 'Robot2@robot.com'
+    },
+    {
+        id: 3,
+        name: 'Robot 3',
+        username: 'Robobo3',
+        email: 'Robot3@robot.com'
+    },
+    {
+        id: 4,
+        name: 'Robot 4',
+        username: 'Robobo4',
+        email: 'Robot4@robot.com'
+    },
+    {
+        id: 5,
+        name: 'Robot 5',
+        username: 'Robobo6',
+        email: 'Robot6@robot.com'
+    },
+    {
+        id: 6,
+        name: 'Robot 6',
+        username: 'Robobo7',
+        email: 'Robot7@robot.com'
+    },
+]
